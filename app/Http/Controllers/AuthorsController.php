49a3db0e554969f49557f827cc7c0f5d0730024e
ramkodgreat <nurudeenakindele8@gmail.com>
2020-08-21 19:12:58 +0100

    first commit

---
+++ app/Http/Controllers/AuthorsController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Author;
+use Illuminate\Http\Request;
+
+class AuthorsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $authors = Author::all();
+        return AuthorsResource::collection($authors);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(createAuthorRequest $request)
+    {
+        $author = Author::create([
+            'name' => $request->input('data.attributes.name'),
+            ]);
+            return (new AuthorsResource($author))->response()
+            ->header('Location', route('authors.show', ['author' =>
+            $author]));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Author $author)
+    {
+        return new AuthorsResource($author);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAuthorRequest $request, Author $author)
+    {
+        $author->update($request->input('data.attributes'));
+        return new AuthorsResource($author);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Author $author)
+    {
+        $author->delete();
+        return response(null, 204);
+    }
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *@test
+     * 
+     */
+    public function BasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
